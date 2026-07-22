fa993ea4ae377ef2ca32e4cff8c7e0deccd9713d
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-07-23 00:24:50 +0600

    feat:homepage created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-export default function Page() {
+export default function page() {
   return (
-    <main>
-      <div>
-        
-        <h1>Hello1!</h1>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-[60vh] text-center">
+      {/* Text */}
+      <h1 className="text-4xl sm:text-6xl font-extrabold tracking-tight bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent mb-4">
+        Welcome to TechBuilt
+      </h1>
+
+      {/* Sub-Text */}
+      <p className="text-lg text-gray-500 max-w-md">Build, Create, Show...</p>
+    </div>
   );
 }
